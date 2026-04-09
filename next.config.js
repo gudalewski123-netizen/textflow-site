09e0219cb126a0aec61272e0ec3f22d344c24b34
Grzegorz Gudalewski <gudalewski123@gmail.com>
2026-04-09 17:49:16 -0500

    Fix environment variable exposure in Next.js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Remove turbopack config - use default Next.js bundler
+  // Expose environment variables to browser
+  env: {
+    NEXT_PUBLIC_GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
